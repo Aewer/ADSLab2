e31c85b339f65c24da57583b2f975e4d61580be0
aewer <slawa.miroshnik.2004@gmail.com>
2023-04-19 20:29:39 +0600

    Changed class Node to type E Added head, tail, size variables

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,14 +1,19 @@
-public class MyLinkedList {
-    private class Node {
-        int val;
-        Node next;
-        Node(int val) {
+public class MyLinkedList<E> {
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+    private static class Node<E> {
+        E val;
+        Node<E> next, previous;
+
+        Node(E val) {
             this.val = val;
             this.next = null;
         }
     }
-    Node head;
     MyLinkedList() {
         this.head = null;
+        this. size = 0;
     }
+
 }
