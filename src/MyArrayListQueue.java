bce88733e7f20dcf7bd9554bb50835e96827986e
aewer <slawa.miroshnik.2004@gmail.com>
2023-05-01 22:02:23 +0600

    Documentation for MyLinkedListQueue and MyArrayListQueue

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,23 +1,41 @@
 public class MyArrayListQueue<T> {
     MyArrayList<T> arrayList;
+    /*
+    Constructor
+     */
     MyArrayListQueue() {
         arrayList = new MyArrayList<>();
     }
+    /*
+    Returns the value at the front of queue
+     */
     public T peek() {
         return arrayList.get(0);
     }
+    /*
+    Adds value at the front of queue
+     */
     public T enqueue(T value) {
         arrayList.add(value);
         return value;
     }
+    /*
+    Removes value at the front of the queue
+     */
     public T dequeue() {
         T value = peek();
         arrayList.remove(0);
         return value;
     }
+    /*
+    Returns the size of the queue
+     */
     public int size() {
         return arrayList.size();
     }
+    /*
+    Returns true if queue is empty and false if it isn't
+     */
     public boolean isEmpty() {
         if (arrayList.size() == 0) {
             return true;
--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,23 +1,41 @@
 public class MyLinkedListQueue<T> {
     MyLinkedList<T> linkedList;
+    /*
+    Constructor
+     */
     MyLinkedListQueue() {
         linkedList = new MyLinkedList<>();
     }
+    /*
+    Returns the value at the front of queue
+     */
     public T peek() {
         return linkedList.get(0);
     }
+    /*
+    Adds value at the front of queue
+     */
     public T enqueue(T value) {
         linkedList.add(value);
         return value;
     }
+    /*
+    Removes value at the front of the queue
+     */
     public T dequeue() {
         T value = peek();
         linkedList.remove(0);
         return value;
     }
+    /*
+    Returns the size of the queue
+     */
     public int size() {
         return linkedList.size();
     }
+    /*
+    Returns true if queue is empty and false if it isn't
+     */
     public boolean isEmpty() {
         if (linkedList.size() == 0) {
             return true;
