1f7b0c5bd841a9697d37fbc9aae2018c7755dd61
aewer <slawa.miroshnik.2004@gmail.com>
2023-05-01 21:59:13 +0600

    Documentation for MyLinkedListStack and MyArrayListStack

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,23 +1,41 @@
 public class MyArrayListStack<T> {
     MyArrayList<T> arrayList;
+    /*
+    Constructor
+     */
     MyArrayListStack() {
         arrayList = new MyArrayList<>();
     }
+    /*
+    Adds value to the top of stack
+     */
     public T push(T value) {
         arrayList.add(value);
         return value;
     }
+    /*
+    Returns value at the top of stack
+     */
     public T peek() {
         return arrayList.get(arrayList.size() - 1);
     }
+    /*
+    Removes and returns the top value in stack
+     */
     public T pop() {
         T value = peek();
         arrayList.remove(arrayList.size() - 1);
         return value;
     }
+    /*
+    Returns the size of the stack
+     */
     public int size() {
         return arrayList.size();
     }
+    /*
+    Returns true if stack is empty and false if it isn't
+     */
     public boolean isEmpty() {
         if (arrayList.size() == 0) {
             return true;
--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,23 +1,41 @@
 public class MyLinkedListStack<T> {
     MyLinkedList<T> linkedList;
+    /*
+    Constructor
+     */
     MyLinkedListStack() {
         linkedList = new MyLinkedList<>();
     }
+    /*
+    Adds value to the top of stack
+     */
     public T push(T value) {
         linkedList.add(value);
         return value;
     }
+    /*
+    Returns value at the top of stack
+     */
     public T peek() {
         return linkedList.get(linkedList.size() - 1);
     }
+    /*
+    Removes and returns the top value in stack
+     */
     public T pop() {
         T value = peek();
         linkedList.remove(linkedList.size() - 1);
         return value;
     }
+    /*
+    Returns the size of the stack
+     */
     public int size() {
         return linkedList.size();
     }
+    /*
+    Returns true if stack is empty and false if it isn't
+     */
     public boolean isEmpty() {
         if (linkedList.size() == 0) {
             return true;
